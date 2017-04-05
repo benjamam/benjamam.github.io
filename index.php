3e8e42ea17d9b7b0ec58b7587f4fe51969117360
benjamam <benjamam@miamioh.edu>
2017-04-05 12:24:37 -0400

    added view

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php require 'partials/top.view.php'; ?>
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+                <span class="hamb-top"></span>
+    			<span class="hamb-middle"></span>
+				<span class="hamb-bottom"></span>
+            </button>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="content text-center">
+                            <h1>Welcome to AgriTRAK</h1>
+                            <h4>A smart farming solution for the future</h4>
+                            <img src="images/sensorNode.jpg" alt="logo">  
+                            <br/><br/>
+                            <p>AgriTRAK is an embedded system project that is designed to support any small scale farming operation by providing a metrics dashboard to its users and automated care to plants.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+    <script src="JS/sidemenu.js"></script>
+</body>
+</html>
